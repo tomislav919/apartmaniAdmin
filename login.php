dde21c95ebb0fbcb962698745efae4adb8802574
Tomislav <tomislav919@gmail.com>
2020-02-20 13:52:58 +0100

    Pathing repaired

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<?php require "../bootstrap.php"; ?>
 <html>
 <head>
   <meta charset="utf-8">
@@ -8,29 +9,30 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="/apartmaniAdmin/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="<?=ROOTPATH?>/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="/apartmaniAdmin/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="<?=ROOTPATH?>/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="/apartmaniAdmin/dist/css/adminlte.min.css">
-  <link rel="stylesheet" href="/apartmaniAdmin/css/custom.css">
+  <link rel="stylesheet" href="<?=ROOTPATH?>/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="<?=ROOTPATH?>/css/custom.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <?php include('include/sessionMessage.php'); ?>
 </head>
 <body class="hold-transition login-page">
-<?php include('include/sessionMessage.php'); ?>
+
 <div class="login-box">
   <div class="login-logo">
-    <a href="/apartmaniAdmin/apartments/apartment1.php"><b>Apartmani</b>Babe</a>
+    <a href="<?=ROOTPATH?>/apartments/apartment1.php"><b>Apartmani</b>Babe</a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
     <div class="card-body login-card-body">
       <p class="login-box-msg">Sign in to start your session</p>
 
-      <form action="/apartmaniAdmin/controllers/loginCheck.php" method="post">
+      <form action="<?=ROOTPATH?>/controllers/loginCheck.php" method="post">
         <div class="input-group mb-3">
           <input name="name" type="name" class="form-control" placeholder="Username">
           <div class="input-group-append">
@@ -60,11 +62,11 @@
 <!-- /.login-box -->
 
 <!-- jQuery -->
-<script src="/apartmaniAdmin/plugins/jquery/jquery.min.js"></script>
+<script src="<?=ROOTPATH?>/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="/apartmaniAdmin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?=ROOTPATH?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="/apartmaniAdmin/dist/js/adminlte.min.js"></script>
+<script src="<?=ROOTPATH?>/dist/js/adminlte.min.js"></script>
 
 </body>
 </html>
